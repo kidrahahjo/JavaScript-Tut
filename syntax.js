ecf572672e78772e886b5e2482fcef4e13c4606e
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-05-15 00:17:58 +0530

    Syntax Introduction

---
+++ syntax.js
@@ -0,0 +1,18 @@
+const firstName = "Hardik";
+const lastName = 'Ojha'
+
+const val = 42
+
+const dec = 42.4
+
+const arr = [
+	'string',
+	42,
+	function(){console.log('hi')},
+]
+
+arr[2]()
+
+for(let i = 0; i < arr.length; i++){
+	console.log(arr[i])
+}
